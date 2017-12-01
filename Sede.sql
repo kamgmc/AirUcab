0fd7e0fea3dd5288812cd19cf0b0002fbaf55fbf
Brs_Tor <boris-augusto@hotmail.com>
2017-12-01 10:38:39 -0400

    Update Sede.sql

--- Sede.sql
+++ Sede.sql
@@ -2,4 +2,4 @@ INSERT INTO public."Sede" (se_nombre, se_area, se_principal, se_lugar) VALUES ('
 INSERT INTO public."Sede" (se_nombre, se_area, se_principal, se_lugar) VALUES ('AirUcab Valencia',47234.465,TRUE,1016);
 INSERT INTO public."Sede" (se_nombre, se_area, se_principal, se_lugar) VALUES ('AirUcab Maracay',11456.14,TRUE,65);
 INSERT INTO public."Sede" (se_nombre, se_area, se_principal, se_lugar) VALUES ('AirUcab Guatire',12312.45,TRUE,1041);
-INSERT INTO public."Sede" (se_nombre, se_area, se_principal, se_lugar) VALUES ('AirUcab Colón',945124.23,TRUE,344);
+INSERT INTO public."Sede" (se_nombre, se_area, se_principal, se_lugar) VALUES ('AirUcab Colón',900,TRUE,344);
