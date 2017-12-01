ddb7b64ab9cf58d8e58f6012068f31b1b5b0fe90
Brs_Tor <boris-augusto@hotmail.com>
2017-11-30 23:37:19 -0400

    Add files via upload

---
+++ Sede.sql
@@ -0,0 +1,5 @@
+INSERT INTO public."Sede" (se_nombre, se_area, se_principal, se_lugar) VALUES ('AirUcab Catia la Mar',500000.23,TRUE,1369);
+INSERT INTO public."Sede" (se_nombre, se_area, se_principal, se_lugar) VALUES ('AirUcab Valencia',47234.465,TRUE,1016);
+INSERT INTO public."Sede" (se_nombre, se_area, se_principal, se_lugar) VALUES ('AirUcab Maracay',11456.14,TRUE,65);
+INSERT INTO public."Sede" (se_nombre, se_area, se_principal, se_lugar) VALUES ('AirUcab Guatire',12312.45,TRUE,1041);
+INSERT INTO public."Sede" (se_nombre, se_area, se_principal, se_lugar) VALUES ('AirUcab Colón',945124.23,TRUE,344);
