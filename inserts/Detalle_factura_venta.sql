fbe9e6fd2c8c24a3f7fb278faf6dfa9d64c957ed
Brs_Tor <boris-augusto@hotmail.com>
2017-12-12 22:03:15 -0400

    Update Detalle_factura_venta.sql

--- inserts/Detalle_factura_venta.sql
+++ inserts/Detalle_factura_venta.sql
@@ -1,6 +1,6 @@
 --DETALLE_FACTURA_VENTA
-INSERT INTO Detalle_factura_venta (dfv_cantidad,dfv_precio,dfv_factura_venta) VALUES (1,1234678.911,1);
-INSERT INTO Detalle_factura_venta (dfv_cantidad,dfv_precio,dfv_factura_venta) VALUES (1,845626.999,2);
-INSERT INTO Detalle_factura_venta (dfv_cantidad,dfv_precio,dfv_factura_venta) VALUES (1,4658412.96,3);
-INSERT INTO Detalle_factura_venta (dfv_cantidad,dfv_precio,dfv_factura_venta) VALUES (1,2658426.11,4);
-INSERT INTO Detalle_factura_venta (dfv_cantidad,dfv_precio,dfv_factura_venta) VALUES (1,9541254.235,5);
+INSERT INTO Detalle_factura_venta (dfv_precio,dfv_factura_venta) VALUES (1234678.911,1);
+INSERT INTO Detalle_factura_venta (dfv_precio,dfv_factura_venta) VALUES (845626.999,2);
+INSERT INTO Detalle_factura_venta (dfv_precio,dfv_factura_venta) VALUES (4658412.96,3);
+INSERT INTO Detalle_factura_venta (dfv_precio,dfv_factura_venta) VALUES (2658426.11,4);
+INSERT INTO Detalle_factura_venta (dfv_precio,dfv_factura_venta) VALUES (9541254.235,5);
