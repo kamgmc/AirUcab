ae6a836af82f527e45583e58d4ed857ca4f9728d
kamgmc <kamgmc@gmail.com>
2017-12-14 10:12:45 -0300

    Update querys.php

--- php/querys.php
+++ php/querys.php
@@ -15,6 +15,44 @@
 		$qry = "DELETE FROM Status WHERE st_id=".$id;
 		return pg_query($conexion, $qry);
 	}
+//Querys de Rol de sistema
+	function insertarRol( $nombre ){
+		$nombre = htmlentities($nombre, ENT_QUOTES);
+		$qry = "INSERT INTO Rol_sistema (sr_nombre) VALUES ('".$nombre."')";
+		return pg_query($conexion, $qry);
+	}
+	function editarRol( $id, $nombre ){
+		$nombre = htmlentities($nombre, ENT_QUOTES);
+		$qry = "UPDATE Rol_sistema SET sr_nombre='".$nombre."' WHERE sr_id=".$id;
+		return pg_query($conexion, $qry);
+	}
+	function eliminarRol( $id ){
+		$nombre = htmlentities($nombre, ENT_QUOTES);
+		$qry = "DELETE FROM Rol_sistema WHERE sr_id=".$id;
+		return pg_query($conexion, $qry);
+	}
+//Querys de Permiso
+	function insertarPermiso( $nombre, $iniciales ){
+		$nombre = htmlentities($nombre, ENT_QUOTES);
+		$iniciales = htmlentities($iniciales, ENT_QUOTES);
+		if(strlen($iniciales) < 3 || strlen($iniciales) > 5)
+			return false;
+		$qry = "INSERT INTO Permiso (pe_nombre, pe_iniciales) VALUES ('".$nombre."','".$iniciales."')";
+		return pg_query($conexion, $qry);
+	}
+	function editarPermiso( $id, $nombre, $iniciales ){
+		$nombre = htmlentities($nombre, ENT_QUOTES);
+		$iniciales = htmlentities($iniciales, ENT_QUOTES);
+		if(strlen($iniciales) < 3 || strlen($iniciales) > 5)
+			return false;
+		$qry = "UPDATE Permiso SET pe_nombre='".$nombre."', pe_iniciales='".$nombre."' WHERE pe_id=".$id;
+		return pg_query($conexion, $qry);
+	}
+	function eliminarPermiso( $id ){
+		$nombre = htmlentities($nombre, ENT_QUOTES);
+		$qry = "DELETE FROM Permiso WHERE pe_id=".$id;
+		return pg_query($conexion, $qry);
+	}
 // Querys de Lugar
 	function insertarLugar( $nombre , $tipo, $lugar ){
 		$nombre = htmlentities($nombre, ENT_QUOTES);
