4b937e9dea42ae73605ac50180cb9428f67ea9b0
kamgmc <kamgmc@gmail.com>
2017-12-14 09:46:02 -0300

    Update querys.php

--- php/querys.php
+++ php/querys.php
@@ -18,15 +18,11 @@
 // Querys de Lugar
 	function insertarLugar( $nombre , $tipo, $lugar ){
 		$nombre = htmlentities($nombre, ENT_QUOTES);
-		if($lugar <= 0)  
-			$lugar = "NULL";
 		$qry = "INSERT INTO Lugar (lu_nombre, lu_tipo, lu_lugar) VALUES ('".$nombre."','".$tipo."',".$lugar.")";
 		return pg_query($conexion, $qry);
 	}
 	function editarLugar( $id, $nombre, $tipo, $lugar ){
 		$nombre = htmlentities($nombre, ENT_QUOTES);
-		if($lugar <= 0)  
-			$lugar = "NULL";
 		$qry = "UPDATE Lugar SET lu_nombre ='".$nombre."', lu_tipo='".$tipo."', lu_lugar=".$lugar." where lu_id=".$id;
 		return pg_query($conexion, $qry);
 	}
