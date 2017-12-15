8f2b59ea028feac1f5085d0dbfb4d7c5fbfa0577
kamgmc <kamgmc@gmail.com>
2017-12-15 01:08:52 -0300

    Update querys.php

--- php/querys.php
+++ php/querys.php
@@ -31,28 +31,6 @@
 		$qry = "DELETE FROM Rol_sistema WHERE sr_id=".$id;
 		return pg_query($conexion, $qry);
 	}
-//Querys de Permiso
-	function insertarPermiso( $nombre, $iniciales ){
-		$nombre = htmlentities($nombre, ENT_QUOTES);
-		$iniciales = htmlentities($iniciales, ENT_QUOTES);
-		if(strlen($iniciales) < 3 || strlen($iniciales) > 5)
-			return false;
-		$qry = "INSERT INTO Permiso (pe_nombre, pe_iniciales) VALUES ('".$nombre."','".$iniciales."')";
-		return pg_query($conexion, $qry);
-	}
-	function editarPermiso( $id, $nombre, $iniciales ){
-		$nombre = htmlentities($nombre, ENT_QUOTES);
-		$iniciales = htmlentities($iniciales, ENT_QUOTES);
-		if(strlen($iniciales) < 3 || strlen($iniciales) > 5)
-			return false;
-		$qry = "UPDATE Permiso SET pe_nombre='".$nombre."', pe_iniciales='".$nombre."' WHERE pe_id=".$id;
-		return pg_query($conexion, $qry);
-	}
-	function eliminarPermiso( $id ){
-		$nombre = htmlentities($nombre, ENT_QUOTES);
-		$qry = "DELETE FROM Permiso WHERE pe_id=".$id;
-		return pg_query($conexion, $qry);
-	}
 //Querys de Rol - permiso
 	function insertarRolPermiso( $rol, $permiso ){
 		$qry = "INSERT INTO Rol_permiso (rp_rol,rp_permiso) VALUES (".$rol.",".$permiso.")";
@@ -81,4 +59,92 @@
 		$qry = "DELETE FROM Lugar where lu_id=".$id;
 		return pg_query($conexion, $qry);
 	}
+// Querys de Sede
+	function insertarSede( $nombre , $area, $principal, $lugar ){
+		$nombre = htmlentities($nombre, ENT_QUOTES);
+		$qry = "INSERT INTO Sede (se_nombre, se_area, se_principal, se_lugar) VALUES ('".$nombre."',".$area.",".$principal.",".$lugar.")";
+		return pg_query($conexion, $qry);
+	}
+	function editarSede( $id, $nombre , $area, $principal, $lugar ){
+		$nombre = htmlentities($nombre, ENT_QUOTES);
+		$qry = "UPDATE Sede SET se_nombre='".$nombre."', se_area=".$area.", se_principal=".$principal.", se_lugar=".$lugar." WHERE se_id=".$id;
+		return pg_query($conexion, $qry);
+	}
+	function eliminarSede( $id ){
+		$qry = "DELETE FROM Sede where se_id=".$id;
+		return pg_query($conexion, $qry);
+	}
+//Query de Zona
+	function insertarZona( $nombre , $tipo, $sede ){
+		$nombre = htmlentities($nombre, ENT_QUOTES);
+		if($tipo != "Ensamblaje" && $tipo != "Prueba")
+			return false;
+		$qry = "INSERT INTO Zona (zo_nombre, zo_tipo, zo_sede) VALUES ('".$nombre."','".$tipo."',".$sede.")";
+		return pg_query($conexion, $qry);
+	}
+	function editarZona( $id, $nombre , $tipo, $sede ){
+		$nombre = htmlentities($nombre, ENT_QUOTES);
+		if($tipo != "Ensamblaje" && $tipo != "Prueba")
+			return false;
+		$qry = "UPDATE Zona SET zo_nombre='".$nombre."', zo_tipo='".$tipo."', zo_sede=".$sede." WHERE zo_id=".$id;
+		return pg_query($conexion, $qry);
+	}
+	function eliminarZona( $id ){
+		$qry = "DELETE FROM Zona where zo_id=".$id;
+		return pg_query($conexion, $qry);
+	}
+//Query de Titulacion
+	function insertarTitulacion( $nombre ){
+		$nombre = htmlentities($nombre, ENT_QUOTES);
+		$qry = "INSERT INTO Titulacion (ti_nombre) VALUES ('".$nombre."')";
+		return pg_query($conexion, $qry);
+	}
+	function editarTitulacion( $id, $nombre ){
+		$nombre = htmlentities($nombre, ENT_QUOTES);
+		$qry = "UPDATE Titulacion SET ti_nombre='".$nombre."' WHERE ti_id=".$id;
+		return pg_query($conexion, $qry);
+	}
+	function eliminarTitulacion( $id ){
+		$qry = "DELETE FROM Titulacion where ti_id=".$id;
+		return pg_query($conexion, $qry);
+	}
+//Cargo
+//Empleado
+//Beneficiario
+//Experiencia
+//Cliente
+//Factura_venta
+//Detalle_factura_venta
+//Proveedor
+//Tipo_contacto
+//Contacto
+//Marca_motor
+//Modelo_motor
+//Modelo_avion
+//Distribucion
+//Submodelo_avion
+//S_avion_m_motor
+//Tipo_ala
+//Tipo_estabilizador
+//Modelo_pieza
+//S_avion_m_pieza
+//Prueba
+//Status_prueba
+//Factura_compra
+//Detalle_factura_compra
+//Tipo_pago
+//Pago
+//Tipo_material
+//T_material_m_pieza
+//Avion
+//Status_avion
+//Motor
+//Status_motor
+//Pieza
+//Status_pieza
+//Material
+//Prueba_material
+//Status_material
+//Prueba_pieza
+//Traslado
 ?>
