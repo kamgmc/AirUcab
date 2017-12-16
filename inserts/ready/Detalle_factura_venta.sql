691e81a9d2e5ee18839dfaf4ed629b1ca27f8009
Brs_Tor <boris-augusto@hotmail.com>
2017-12-15 23:18:13 -0400

    Delete Detalle_factura_venta.sql

--- inserts/ready/Detalle_factura_venta.sql
+++
@@ -1,6 +0,0 @@
---DETALLE_FACTURA_VENTA
-INSERT INTO Detalle_factura_venta (dfv_precio,dfv_factura_venta) VALUES (1234678.911,1);
-INSERT INTO Detalle_factura_venta (dfv_precio,dfv_factura_venta) VALUES (845626.999,2);
-INSERT INTO Detalle_factura_venta (dfv_precio,dfv_factura_venta) VALUES (4658412.96,3);
-INSERT INTO Detalle_factura_venta (dfv_precio,dfv_factura_venta) VALUES (2658426.11,4);
-INSERT INTO Detalle_factura_venta (dfv_precio,dfv_factura_venta) VALUES (9541254.235,5);
