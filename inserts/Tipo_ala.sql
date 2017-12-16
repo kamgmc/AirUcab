bc3bb9e8661791e88b3d388b5b41002dd28e5019
Brs_Tor <boris-augusto@hotmail.com>
2017-12-15 19:26:28 -0900

    Add files via upload

---
+++ inserts/Tipo_ala.sql
@@ -0,0 +1,13 @@
+--TIPO_ALA
+INSERT INTO Tipo_ala (wt_nombre) VALUES ('Recta');
+INSERT INTO Tipo_ala (wt_nombre) VALUES ('Trapezoidal');
+INSERT INTO Tipo_ala (wt_nombre) VALUES ('Elíptica');
+INSERT INTO Tipo_ala (wt_nombre) VALUES ('Flecha');
+INSERT INTO Tipo_ala (wt_nombre) VALUES ('Flecha invertida');
+INSERT INTO Tipo_ala (wt_nombre) VALUES ('Doble flecha');
+INSERT INTO Tipo_ala (wt_nombre) VALUES ('Flecha variable');
+INSERT INTO Tipo_ala (wt_nombre) VALUES ('Delta');
+INSERT INTO Tipo_ala (wt_nombre) VALUES ('Delta con canard');
+INSERT INTO Tipo_ala (wt_nombre) VALUES ('Delta con timones');
+INSERT INTO Tipo_ala (wt_nombre) VALUES ('Doble delta');
+INSERT INTO Tipo_ala (wt_nombre) VALUES ('Ojival');
---
+++ inserts/Tipo_estabilizador.sql
@@ -0,0 +1,7 @@
+--TIPO_ESTABILIZADOR
+INSERT INTO Tipo_estabilizador (et_nombre) VALUES ('Estandar');
+INSERT INTO Tipo_estabilizador (et_nombre) VALUES ('T');
+INSERT INTO Tipo_estabilizador (et_nombre) VALUES ('Cruz');
+INSERT INTO Tipo_estabilizador (et_nombre) VALUES ('Dos verticales');
+INSERT INTO Tipo_estabilizador (et_nombre) VALUES ('Tres verticales');
+INSERT INTO Tipo_estabilizador (et_nombre) VALUES ('V');
