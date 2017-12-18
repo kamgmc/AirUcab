964e6e753287162ce9fa7e4d2c644d743a534fef
kamgmc <kamgmc@gmail.com>
2017-12-17 21:57:26 -0300

    Update insert.sql

--- sql/insert.sql
+++ sql/insert.sql
@@ -2040,8 +2040,8 @@ INSERT INTO Tipo_estabilizador (et_nombre) VALUES ('V');
 INSERT INTO Status (st_nombre) VALUES ('Listo');
 INSERT INTO Status (st_nombre) VALUES ('En Proceso');
 INSERT INTO Status (st_nombre) VALUES ('Evaluación');
-INSERT INTO Status (st_nombre) VALUES ('Status 4');
-INSERT INTO Status (st_nombre) VALUES ('Status 5');
+INSERT INTO Status (st_nombre) VALUES ('Ensamblando');
+INSERT INTO Status (st_nombre) VALUES ('Pruebas');
 --AVION
 INSERT INTO Avion (a_fecha_ini,a_fecha_fin,a_precio,a_submodelo_avion,a_distribucion,a_factura_venta) VALUES ('2017-05-21','2017-12-21',921445.65,14,23,1);
 INSERT INTO Avion (a_fecha_ini,a_fecha_fin,a_precio,a_submodelo_avion,a_distribucion,a_factura_venta) VALUES ('2017-05-21','2017-12-21',126042,18,25,2);
@@ -2053,9 +2053,10 @@ INSERT INTO Status_avion (sa_fecha_ini,sa_fecha_fin,sa_avion,sa_status) VALUES (
 INSERT INTO Status_avion (sa_fecha_ini,sa_fecha_fin,sa_avion,sa_status) VALUES ('2017-11-21','2017-11-21',1,1);
 INSERT INTO Status_avion (sa_fecha_ini,sa_fecha_fin,sa_avion,sa_status) VALUES ('2017-05-21','2017-11-20',2,2);
 INSERT INTO Status_avion (sa_fecha_ini,sa_fecha_fin,sa_avion,sa_status) VALUES ('2017-11-21','2017-11-21',2,1);
-INSERT INTO Status_avion (sa_fecha_ini,sa_avion,sa_status) VALUES ('2017-05-21',3,2);
+INSERT INTO Status_avion (sa_fecha_ini,sa_avion,sa_status) VALUES ('2017-12-21',5,2);
 INSERT INTO Status_avion (sa_fecha_ini,sa_avion,sa_status) VALUES ('2017-05-21',4,2);
 INSERT INTO Status_avion (sa_fecha_ini,sa_avion,sa_status) VALUES ('2017-12-21',5,2);
+INSERT INTO Status_avion (sa_fecha_ini,sa_avion,sa_status) VALUES ('2017-12-21',3,2);
 --MOTOR
 INSERT INTO Motor (mo_fecha_ini,mo_fecha_fin,mo_modelo_motor,mo_avion) VALUES ('2017-05-21','2017-11-21',14,1);
 INSERT INTO Motor (mo_fecha_ini,mo_fecha_fin,mo_modelo_motor,mo_avion) VALUES ('2017-05-21','2017-11-21',15,1);
