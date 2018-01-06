801944e7f26738f048677d054222acf89185dd5d
Brs_Tor <boris-augusto@hotmail.com>
2018-01-06 03:44:39 -0400

    Update clientes.php

--- php/clientes.php
+++ php/clientes.php
@@ -104,7 +104,7 @@ if( !in_array("cl_r", $permiso)){
 							<a href="piezas.php"> <i class="fa fa-puzzle-piece " aria-hidden="true"></i>Piezas </a>
 						</li>
                         <?php } ?>
-                        <?php if( in_array("m_r", $permiso) ) { ?>
+                        <?php if( in_array("m_r", $permiso) || in_array("tm_r", $permiso) ) { ?>
                         <li>
 							<a href="materiales.php"> <i class="fa fa-server " aria-hidden="true"></i>Materiales </a>
 						</li>
