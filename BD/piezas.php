2baa98213735fee82cc6939993a4643f3f826588
Brs_Tor <boris-augusto@hotmail.com>
2018-01-11 03:36:00 -0400

    Update piezas.php

--- BD/piezas.php
+++ BD/piezas.php
@@ -39,7 +39,7 @@ if( !in_array("p_r", $permiso) && !in_array("pm_r", $permiso) && !in_array("wt_r
 	<!-- Custom stylesheet - for your changes-->
 	<link rel="stylesheet" href="css/custom.css">
 	<!-- Favicon-->
-	<link rel="shortcut icon" href="favicon.png"> </head>
+	<link rel="shortcut icon" href="img/airucab.ico"> </head>
 
 <body>
 	<div class="page home-page">
